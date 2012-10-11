cbb9a7faa70231e9ea50fb66da6756fd4d50d03b
Jonathan Balsano <jrbalsano@gmail.com>
2012-10-11 19:55:44 -0400

    Working and centered, missing scrolling

--- js/pomotodo.js
+++ js/pomotodo.js
@@ -4,7 +4,7 @@ PTD.Task = Backbone.Model.extend({
 });
 
 PTD.Tasks = Backbone.Collection.extend({
-  model: PTD.Task,
+  model: PTD.Task
 });
 
 PTD.AddTaskView = Backbone.View.extend({
@@ -22,7 +22,8 @@ PTD.AddTaskView = Backbone.View.extend({
       if (textbox.val() == "")
         return false;
       this.collection.create({
-       text: textbox.val(),
+        text: textbox.val(),
+        complete: false
       });
       textbox.val("");
     }
@@ -34,17 +35,73 @@ PTD.IncompleteTasksView = Backbone.View.extend({
   id: "incomplete",
 
   initialize: function() {
-    this.collection.on("add", this.render, this);
+    this.collection.on("add", this.renderNew, this);
+    this.collection.on("change:complete", this.renderComplete, this);
   },
 
-  render: function(task) {
-    console.log("Fired!");
-    var html = Mustache.render($("#inc-template").html(), task.attributes);
+  events: {
+    "click .mark_complete": "markComplete"
+  },
+
+  renderNew: function(task) {
+    taskAtts = task.attributes;
+    taskAtts.cid = task.cid;
+    var html = Mustache.render(PTD.incomplete_template, taskAtts);
     this.$el.prepend(html);
+    PTD.centerAlignMiddle();
+  },
+
+  renderComplete: function(task) {
+    if(task.get("complete") == true) {
+      cid = task.cid;
+      $(".incomplete#" + cid).remove();
+      PTD.centerAlignMiddle();
+    }
+  },
+
+  markComplete: function(e) {
+    e.preventDefault();
+    data = e.currentTarget.dataset;
+    task = this.collection.getByCid(data.cid);
+    task.set("complete", true);
+  } 
+});
+
+PTD.CompleteTasksView = Backbone.View.extend({
+  tagName: "ul",
+  id: "complete",
+
+  initialize: function() {
+    this.collection.on("change:complete", this.renderNew, this);
+  },
+
+  renderNew: function(task) {
+    taskAtts = task.attributes;
+    taskAtts.cid = task.cid;
+    var html = Mustache.render(PTD.complete_template, taskAtts);
+    this.$el.append(html);
+    PTD.centerAlignMiddle();
   }
 });
 
+PTD.centerAlignMiddle = function() {
+  var $vc = $("#vcenter");
+  var formHeight = $("#middle").outerHeight();
+  var viewHeight = $(window).height();
+  var formTop = (viewHeight - formHeight)/2;
+  var divTop = formTop - $("#complete").outerHeight();
+  $vc.css("top", divTop);
+};
+
+PTD.setListMaxHeight = function() {
+  $("ul").css("max-height", ($(window).height() - $("#middle").outerHeight())/2);
+};
+
 $(document).ready(function () {
+  PTD.incomplete_template = $("#inc-template").html();
+  PTD.complete_template = $("#com-template").html();
+  PTD.centerAlignMiddle();
+
   var myTasks = new PTD.Tasks();
   var myAddTaskView = new PTD.AddTaskView({
     collection: myTasks,
@@ -53,5 +110,10 @@ $(document).ready(function () {
   var myIncompleteTasksView = new PTD.IncompleteTasksView({
     collection: myTasks,
     el: $("#incomplete")
+  });
+
+  var myCompleteTasksView = new PTD.CompleteTasksView({
+    collection: myTasks,
+    el: $("#complete")
   });
 });
